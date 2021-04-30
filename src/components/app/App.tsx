39705778fc0870bbd0f26d6c6d54c2909760aa1e
Brandon Frie <brandon.frie@aviture.us.com>
2021-04-30 14:21:29 -0700

    feat(ui): added info panel and map updates

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,18 +1,56 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import { MapView } from '../map-view';
 import { MapCurrentConditions } from '../current-conditions';
 import { LatLng, Map } from 'leaflet';
+import { makeStyles } from '@material-ui/core';
+
+let watchId: number;
+
+const useStyles = makeStyles({
+    crosshair: {
+        left: 'calc(50% - 1em)',
+        top: 'calc(50% - 1em)',
+        position: 'absolute',
+        width: '2em',
+        height: '2em',
+        zIndex: 10000,
+        textAlign: 'center',
+        fontWeight: 'normal',
+        fontSize: '2em',
+        color: '#222',
+        textShadow: '1px 1px 3px #fff'
+    }
+});
 
 function App(): JSX.Element {
     const [map, setMap] = useState<Map | null>(null);
-    const displayMap = useMemo(() => <MapView initialLocation={new LatLng(31.54721611257426, -110.22094826016131)} onWhenCreated={setMap}/>, []);
+    const [position, setPosition] = useState<GeolocationPosition>();
+    const displayMap = useMemo(() => {
+        return <MapView
+            initialLocation={new LatLng(position?.coords.latitude ?? 0, position?.coords.longitude ?? 0)}
+            onWhenCreated={setMap} />;
+    }, [position]);
 
-    console.log(map);
+    const classes = useStyles();
+
+    useEffect(() => {
+        watchId = navigator.geolocation.watchPosition((pos) => {
+            setPosition(pos);
+        }
+        , (error) => {
+            console.error(error);
+        });
+
+        return () => {
+            navigator.geolocation.clearWatch(watchId);
+        };
+    }, []);
 
     return (
         <div className="App">
-            {displayMap}
+            <div className={classes.crosshair}>+</div>
+            {position ? displayMap : 'Locating position...'}
             {map ? <MapCurrentConditions map={map} /> : ''}
         </div>
     );
--- src/components/current-conditions/CurrentConditions.tsx
+++ src/components/current-conditions/CurrentConditions.tsx
@@ -2,22 +2,38 @@ import { LatLng } from 'leaflet';
 import React, { useEffect, useState } from 'react';
 import { getCurrentConditions } from '../../wxapi/functions';
 import { ICurrentConditions } from '../../wxapi/ICurrentConditions';
-import './CurrentConditions.css';
+import Card from '@material-ui/core/Card';
+import { CardContent, CardHeader, makeStyles, Typography } from '@material-ui/core';
 
 const ctof = (celsius: number|undefined): number => {
-    if (!celsius) {
+    if (celsius === undefined) {
         return NaN;
     }
 
-    return Math.round(celsius * 5 / 9 + 32);
+    return Math.round(celsius * 9 / 5 + 32);
 };
 
 type CurrentConditionsProps = {
     point: LatLng
 }
 
+const useStyles = makeStyles({
+    root: {
+        padding: '1em',
+        position: 'absolute',
+        top: '2em',
+        right: '2em',
+        height: '50vh',
+        width: '25vw',
+        zIndex: 1000,
+        backgroundColor: 'rgba(64, 64, 64, 0.85)',
+        color: '#DDD'
+    }
+});
+
 export const CurrentConditions = ({point}: CurrentConditionsProps): JSX.Element => {
     const [currentConditions, setCurrentConditions] = useState<ICurrentConditions | null>(null);
+    const classes = useStyles();
 
     useEffect(() => {
         getCurrentConditions(point.lat, point.lng).then(data => {
@@ -28,11 +44,35 @@ export const CurrentConditions = ({point}: CurrentConditionsProps): JSX.Element 
             setCurrentConditions(null);
         });
     }, [point]);
-    return (
-        <div className="current-conditions">
-            Conditions Report for {currentConditions?.properties.stationName}<br />
-            Temperature: {ctof(currentConditions?.properties.temperature.value)} &deg;F<br />
-            <img src={currentConditions?.properties.icon} alt={currentConditions?.properties.textDescription} />
-        </div>
-    );
+
+    if (currentConditions) {
+        const {
+            temperature,
+            relativeHumidity,
+            icon,
+            textDescription,
+            stationName
+        } = currentConditions.properties;
+
+        return (
+            <Card className={classes.root} elevation={6}>
+                <CardHeader title="Current Conditions Report" />
+                <CardContent>
+                    <Typography>{stationName}</Typography>
+                    <Typography>Temperature: {ctof(temperature.value)} &deg;F</Typography>
+                    <Typography>Humidity: {Math.round(relativeHumidity.value)}%</Typography>
+                    <img src={icon} alt={textDescription} />
+                </CardContent>
+            </Card>
+        );
+    } else {
+        return (
+            <Card className={classes.root} elevation={6}>
+                <CardHeader title="Error" />
+                <CardContent>
+                    <Typography>Encountered an error while retrieving current conditions.</Typography>
+                </CardContent>
+            </Card>
+        );
+    }
 };
